ced38958bcf2e274c03f0d7ac97c0c0c374ff174
mitoa89 <mitoa89@gmail.com>
2016-10-11 22:27:00 +0900

    changed

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 var express = require('express');
 var mongoose = require("mongoose");
-var bodyParser = require("body-parser"); // 1
-var methodOverride = require("method-override"); // 1
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-var app = express();;
+var app = express();
 
 // DB setting
-mongoose.connect(process.env.MONGO_DB); // 1
-var db = mongoose.connection; // 2
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
 
 db.once("open", function(){
  console.log("DB connected");
 });
-// 4
+
 db.on("error", function(err){
  console.log("DB ERROR : ", err);
 });
 
 app.set("view engine","ejs");
 app.use(express.static(__dirname));
-app.use(bodyParser.json()); // 2
-app.use(bodyParser.urlencoded({extended:true})); // 3
-app.use(methodOverride("_method")); // 2
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
-app.use("/", require("./routes/home")); //1
+app.use("/", require("./routes/home"));
 
 app.listen(3000, function(){
   console.log("server on");

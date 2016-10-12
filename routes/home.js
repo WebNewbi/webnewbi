eda53db27ffbccd90ba2b127134581f125908084
musang33 <musang33@nate.com>
2016-10-12 22:56:49 +0900

    check db

--- routes/home.js
+++ routes/home.js
@@ -4,32 +4,39 @@ var Travel = require("../models/travels");
 var router = express.Router();
 
 router.get("/", function(req, res) {
-  Travel.find({}, function(err, travels){
-    if(err) return res.json(err);
-    res.render("index", {travels:travels});
-  });
+    Travel.find({}, function(err, travels) {
+        if (err) return res.json(err);
+        res.render("index", {
+            travels: travels
+        });
+    });
 });
 
-router.get("/signin", function(req, res){
- res.render("signin");
+router.get("/signin", function(req, res) {
+    res.render("signin");
 });
-router.post("/signin", function(req, res){
- Member.create(req.body, function(err, member){
-  if(err) return res.json(err);
-  res.redirect("/");
- });
+router.post("/signin", function(req, res) {
+    Member.findOne({
+        email: req.body.email
+    }, function(err, member) {
+        if (err)
+            Member.create(req.body, function(err, member) {
+                if (err) return res.json(err);
+                res.redirect("/");
+            });
+    });
 });
 
 // Contacts - New // 8
-router.get("/new", function(req, res){
- res.render("new");
+router.get("/new", function(req, res) {
+    res.render("new");
 });
 // Contacts - create // 9
-router.post("/new", function(req, res){
- Travel.create(req.body, function(err, travel){
-  if(err) return res.json(err);
-  res.redirect("/");
- });
+router.post("/new", function(req, res) {
+    Travel.create(req.body, function(err, travel) {
+        if (err) return res.json(err);
+        res.redirect("/");
+    });
 });
 
 module.exports = router;

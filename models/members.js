6f3b8f5cddf18523c4518ed0f14cc9563a989ae4
mitoa89 <mitoa89@gmail.com>
2016-10-12 21:07:00 +0900

    changed

--- models/members.js
+++ models/members.js
@@ -1,6 +1,8 @@
 var mongoose = require("mongoose");
+
 var memberSchema = mongoose.Schema({
  email:{type:String, required:true, unique:true},
+ password:{}
 });
 var Member = mongoose.model("member", memberSchema); //5
 

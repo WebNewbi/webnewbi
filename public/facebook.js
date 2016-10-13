dc6f6988cec6c5d02280adbe2a33c773f9bca13a
mitoa89 <mitoa89@gmail.com>
2016-10-13 20:18:46 +0900

    changed.

--- public/facebook.js
+++ public/facebook.js
@@ -74,5 +74,6 @@
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
+      console.log(JSON.stringify(response));
     });
   }

438cf0207d49fda4e8de97ebe52a25f1cc8caf08
musang33 <musang33@nate.com>
2016-11-06 14:02:07 +0900

    ...

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,24 +1,26 @@
-var express   = require('express');
-var Schedule  = require("../models/schedule");
-var ScheduleUtil = require("../public/module/schedule");
+var express = require('express');
+var Schedule = require("../models/schedule");
+var Geocode = require("../models/geocode");
+var Links = require("../models/link");
+var ScheduleUtil = require("../public/module/util");
 var router = express.Router();
 
 // new
 router.get("/new", function(req, res) {
-    res.render("new");
-})
-.post("/new", function(req, res) {
-    ScheduleUtil.createSchedule( req, res);
-});
+        res.render("new");
+    })
+    .post("/new", function(req, res) {
+        ScheduleUtil.createSchedule(req, res);
+    });
 
 // search
 router.post("/search", function(req, res) {
-  Schedule.find({}, function(err, travels) {
-      if (err) return res.json(err);
-      res.render("index", {
-          travels: travels,
-      });
-  });
+    Schedule.find({}, function(err, travels) {
+        if (err) return res.json(err);
+        res.render("index", {
+            travels: travels,
+        });
+    });
 });
 
 // edit

6c4db876e82e59a125f8090fe04aa5c81b5ffab4
mitoa89 <mitoa89@gmail.com>
2016-11-07 21:53:25 +0900

    tag added

--- public/module/schedule.js
+++ public/module/schedule.js
@@ -14,7 +14,7 @@ util.createSchedule = function ( req, res ){
      end      : req.body.end,
      comment  : req.body.comment
    };
-   
+
     var promise = Schedule.create(newSchedule, function(err, schedule) {
             if (err) return res.json(err);
 
@@ -24,47 +24,22 @@ util.createSchedule = function ( req, res ){
                                     { 'city.name' : req.body.city,
                                       'links' : schedule._id }};
             var option = { upsert : true };
-            Links.findOneAndUpdate( cityLink, update, option,  function(err, schedule) {
+            Links.findOneAndUpdate( cityLsink, update, option,  function(err, schedule) {
                     if (err) return res.json(err);
                   });
 
-
-/*
             for( var tagElement in req.body.tags ){
-              var tagLink  = { tag : tagElement };
-              util.attachLink( tagLink, schedule, res );
+              var tagLink = { 'tag' : tagElement };
+              var updateTag = { $setOnInsert : {'tag' : tagElement },
+                                     $addToSet :{'links' : schedule._id }};
+             Links.findOneAndUpdate(tagLink, updateTag, option, function(err, schedule) {
+                                             if (err) return res.json(err);
+                                           });
             }
-*/
             // callback이 안와도 완료페이지로 redirect시도
         });
 
 
 };
 
-/*
-util.attachLink = function( json , added, schedule, res){
-  Links.findOne( json ).exec(function(err, linkSchema){
-      if (err) return res.json(err);
-      if (linkSchema!==null){
-        util.addLink( linkSchema, schedule );
-      }
-      else {
-        util.createLink( json, schedule, res );
-      }
-  });
-
-};
-
-util.addLink = function (linkSchema, added, schedule ){
-  linkSchema.push( added );
-};
-
-util.createLink = function (json, added, schedule, res ){
-    json.links = [schedule._id];
-    Links.create( json, function( err, link ){
-      if (err) return res.json(err);
-    });
-};
-
-*/
 module.exports = util;

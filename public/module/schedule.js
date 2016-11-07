111b18e4b0ede3211617a1b51b82712beb66e589
musang33 <musang33@nate.com>
2016-11-07 20:16:53 +0900

    ...

--- public/module/schedule.js
+++ public/module/schedule.js
@@ -1,58 +1,61 @@
-var Schedule  = require("../../models/schedule");
-var Geocode   = require("../../models/geocode");
-var Links     = require("../../models/link");
+var Schedule = require("../../models/schedule");
+var Geocode = require("../../models/geocode");
+var Links = require("../../models/link");
 
 var util = {};
 
-util.createSchedule = function ( req, res ){
+util.createSchedule = function(req, res) {
 
-   var newSchedule = {
-     geocode  : req.body.cityCode,
+    var newSchedule = {
+        geocode: req.body.cityCode,
 
-     tags     : req.body.tags,
-     start    : req.body.start,
-     end      : req.body.end,
-     comment  : req.body.comment
-   };
+        tags: req.body.tags,
+        start: req.body.start,
+        end: req.body.end,
+        comment: req.body.comment
+    };
 
     var promise = Schedule.create(newSchedule, function(err, schedule) {
-            if (err) return res.json(err);
+        if (err) return res.json(err);
 
-            var geocodeLink = { geocode : schedule.geocode };
-            util.attachLink( geocodeLink );
+        var geocodeLink = {
+            geocode: schedule.geocode
+        };
+        util.attachLink(geocodeLink);
 
-            var tagLink  = { tag : Schedule.tag };
-            util.attachLink( tagLink );
+        var tagLink = {
+            tag: Schedule.tag
+        };
+        util.attachLink(tagLink);
 
-            // callback이 안와도 완료페이지로 redirect시도
-        });
+        // callback이 안와도 완료페이지로 redirect시도
+    });
 
-
-};
-
-
-util.attachLink = function( json ){
-  Links.findOne( json ).exec(function(err, linkSchema){
-      if (err) return res.json(err);
-      if (linkSchema!==null){
-        util.addLink( linkSchema );
-      }
-      else {
-        util.createLink( json, linkSchema );
-      }
-  });
 
 };
 
-util.addLink = function (linkSchema){
-  link.links.push( linkSchema._id );
+
+util.attachLink = function(json) {
+    Links.findOne(json).exec(function(err, linkSchema) {
+        if (err) return res.json(err);
+        if (linkSchema !== null) {
+            util.addLink(linkSchema);
+        } else {
+            util.createLink(json, linkSchema);
+        }
+    });
+
 };
 
-util.createLink = function (json, linkSchema){
+util.addLink = function(linkSchema) {
+    link.links.push(linkSchema._id);
+};
+
+util.createLink = function(json, linkSchema) {
     json.links = [linkSchema._id];
 
-    Links.create( json, function( err, link ){
-      if (err) return res.json(err);
+    Links.create(json, function(err, link) {
+        if (err) return res.json(err);
     });
 };
 

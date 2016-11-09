fcfb5077435637fed285b299aaf903ededee2608
musang33 <musang33@nate.com>
2016-11-09 20:19:00 +0900

    .

--- models/link.js
+++ models/link.js
@@ -1,6 +1,7 @@
 var mongoose = require("mongoose");
 
 var linkSchema = mongoose.Schema({
+<<<<<<< HEAD
     city: {
         geocode: {
             type: String,
@@ -22,6 +23,27 @@ var linkSchema = mongoose.Schema({
                 return v.length > 0;
             },
             message: '{VALUE} is empty!'
+=======
+  city : { geocode : {
+                type : String,
+                 required : true,
+                 validate: {
+                      validator: function(v) {
+                        return v.length > 0;
+                      },
+                      message: '{VALUE} is empty!'
+                    },},
+            name :  [String]
+          },
+  tag : {
+      type : String,
+      lowercase: true,
+      validate: {
+          validator: function(v) {
+            return v.length > 0;
+          },
+          message: '{VALUE} is empty!'
+>>>>>>> e9c971d956d365e38c281a7050610c11d013ab69
         },
     },
 
